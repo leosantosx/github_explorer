28dc5cf1bc4c24267ce8ba3b002bb732a663957d
leosantosx <leonardosantos0077@gmail.com>
2020-12-02 22:38:11 -0300

    Adicionando style components

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Routes from './routes'
 
+import GlobalStyle from './styles/global'
+
 const App: React.FC = () => {
     return (
         <>
+            <GlobalStyle />
             <BrowserRouter>
                 <Routes />
             </BrowserRouter>
